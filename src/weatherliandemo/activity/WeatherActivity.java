37b15159c5c694da4c5659957cd87a3a35b9462c
cwx <892787782@qq.com>
2015-10-07 01:10:20 +0800

    增加后台自动更新天气的功能。

--- src/weatherliandemo/activity/WeatherActivity.java
+++ src/weatherliandemo/activity/WeatherActivity.java
@@ -1,6 +1,7 @@
 package weatherliandemo.activity;
 
 import weatherliandemo.app.R;
+import weatherliandemo.service.AutoUpdateService;
 import weatherliandemo.util.HttpCallbackListener;
 import weatherliandemo.util.HttpUtil;
 import weatherliandemo.util.Utility;
@@ -189,6 +190,9 @@ public class WeatherActivity extends Activity implements OnClickListener {
 		currentDateText.setText(prefs.getString("current_date", ""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		
+		Intent intent = new Intent(this, AutoUpdateService.class);
+		startService(intent);
 	}
 	
 	/**
